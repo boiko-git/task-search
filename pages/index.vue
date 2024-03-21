26b62cb63acac7a9cda06a275ee94cf28f4344d4
kazantip916 <boikoroman146@gmail.com>
2024-03-21 13:01:58 +0200

    added task search

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="page">
+      <instantSearch />
+  </div>
+</template>
+  
+<script>
+import instantSearch from "~/components/InstantSearch.vue";
+
+export default {
+  name: 'Main',
+
+  components: {  
+    instantSearch
+  }
+}
+</script>
+  
